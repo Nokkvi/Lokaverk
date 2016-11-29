d6a8b73f092f9bb575e000282be323cafe5fd912
Nokkvi <nos1@hi.is>
2016-11-29 13:23:09 +0000

    tv station

--- app.js
+++ app.js
@@ -1,56 +1,16 @@
 var express = require('express');
-var mysql = require('mysql')
-var path = require('path');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var index = require('./routes/index');
-var about = require('./routes/about');
-
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'me',
-  password : 'secret',
-  database : 'my_db'
-});
-
 var app = express();
 
-app.locals.points = "8,652";
-app.locals.videodata = require('./videodata.json');
+app.set('port', (process.env.PORT || 3000));
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname+'/public');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', index);
-app.use('/about', about);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.get('/', function(request, response) {
+    response.render('index');
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.listen(app.get('port'), function() {
+    console.log('Node is running on port: ', app.get('port'));
 });
 
-module.exports = app;
